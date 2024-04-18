d8d15d4a03c61645de87bfa9b2f0fa4ebe82a74e
Diego Mascarenhas <diego@revisionalpha.com>
2024-04-18 12:26:05 +0200

    History GlobalScope

--- app/Models/History.php
+++ app/Models/History.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class History extends Model
 {
 	use HasFactory;
 
 	protected $table = 'history';
+
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope('UsersMessages', function (Builder $builder)
+		{
+			$builder->where('options', '{}')
+				->where('answer', 'not like', '_event_voice_note__%');
+		});
+	}
 }
