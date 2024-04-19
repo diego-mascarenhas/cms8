832fe1a1640f12af8a755778bb3a109dc3b75fbf
Diego Mascarenhas <diego@revisionalpha.com>
2024-04-19 18:15:56 +0200

    ApiAuthController

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,6 @@ use App\Http\Controllers\ApiAuthController;
 |
 */
 
-Route::post('/auth/register', [ApiAuthController::class, 'register']);
-Route::post('/auth/login', [ApiAuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request)
-{
-	return $request->user();
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 {
 	return $request->user();
@@ -34,14 +26,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 Route::post('/webhooks', [WebhookController::class, 'store']);
 Route::post('/trade', [TradeDataController::class, 'store']);
 
-// Route::group(['prefix' => 'auth'], function ()
-// {
-// 	Route::post('login', [AuthController::class, 'login']);
-// 	Route::post('register', [AuthController::class, 'register']);
+Route::group(['prefix' => 'auth'], function ()
+{
+	Route::post('login', [AuthController::class, 'login']);
+	Route::post('register', [AuthController::class, 'register']);
 
-// 	Route::group(['middleware' => 'auth:api'], function ()
-// 	{
-// 		Route::get('logout', [AuthController::class, 'logout']);
-// 		Route::get('user', [AuthController::class, 'user']);
-// 	});
-// });
+	Route::group(['middleware' => 'auth:api'], function ()
+	{
+		Route::get('logout', [AuthController::class, 'logout']);
+		Route::get('user', [AuthController::class, 'user']);
+	});
+});
+
+Route::post('/mobile/register', [ApiAuthController::class, 'register']);
+Route::post('/mobile/login', [ApiAuthController::class, 'login']);
