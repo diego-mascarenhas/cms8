4106858b2341ad0072bc0bf4436a545886c57cd9
Diego Mascarenhas <diego@revisionalpha.com>
2024-04-19 15:16:37 +0200

    Update api routes for API authentication

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\trade\TradeDataController;
 use App\Http\Controllers\sys\WebhookController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,14 @@ use App\Http\Controllers\AuthController;
 |
 */
 
+Route::post('/auth/register', [ApiAuthController::class, 'register']);
+Route::post('/auth/login', [ApiAuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request)
+{
+	return $request->user();
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 {
 	return $request->user();
@@ -25,14 +34,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 Route::post('/webhooks', [WebhookController::class, 'store']);
 Route::post('/trade', [TradeDataController::class, 'store']);
 
-Route::group(['prefix' => 'auth'], function ()
-{
-	Route::post('login', [AuthController::class, 'login']);
-	Route::post('register', [AuthController::class, 'register']);
+// Route::group(['prefix' => 'auth'], function ()
+// {
+// 	Route::post('login', [AuthController::class, 'login']);
+// 	Route::post('register', [AuthController::class, 'register']);
 
-	Route::group(['middleware' => 'auth:api'], function ()
-	{
-		Route::get('logout', [AuthController::class, 'logout']);
-		Route::get('user', [AuthController::class, 'user']);
-	});
-});
+// 	Route::group(['middleware' => 'auth:api'], function ()
+// 	{
+// 		Route::get('logout', [AuthController::class, 'logout']);
+// 		Route::get('user', [AuthController::class, 'user']);
+// 	});
+// });
