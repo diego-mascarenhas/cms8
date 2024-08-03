b7337838d309e8ed6283c904db676dcc7bb670a2
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2024-08-03 20:43:04 +0200

    Styles

--- public/js/openai.js
+++ public/js/openai.js
@@ -1,100 +1,98 @@
-$(document).ready(function() {
-    // Borrar los datos del almacenamiento local al recargar la página
-    localStorage.clear();
-
-    // No pedir claves, asumir que están configuradas en el backend y no se necesitan en el frontend
-    // var apiKey = prompt('Ingresa tu clave de API Open AI.');
-    // var elevenApiKey = prompt('Ingresa tu clave de Eleven API. (Si quieres que el bot hable. Si no déjala en blanco.)');
-
-    // Almacenar las claves en el almacenamiento local
-    // Esto asume que las claves se configurarán correctamente en el backend y no se necesitarán aquí
-    // localStorage.setItem('apiKey', apiKey);
-    // localStorage.setItem('elevenApiKey', elevenApiKey);
-
-    var chatMessages = $('#chat-messages'); // Elemento HTML para mostrar los mensajes del chat
-    var chatInput = $('#chat-input'); // Elemento HTML para el campo de entrada del chat
-    var chatForm = $('#chat-form'); // Elemento HTML para el formulario del chat
-    var chatHistory = []; // Variable para almacenar el historial de mensajes
-
-    chatForm.submit(function(event) {
-        event.preventDefault();
-        var message = chatInput.val().trim();
-    
-        if (message !== '') {
-            sendMessage(message);
-            chatInput.val('');
-        }
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(self, function() {
+return /******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/openai.js ***!
+  \********************************/
+$(document).ready(function () {
+  localStorage.clear();
+  var chatMessages = $('#chat-messages');
+  var chatInput = $('#chat-input');
+  var chatForm = $('#chat-form');
+  var chatHistory = [];
+  chatForm.submit(function (event) {
+    event.preventDefault();
+    var message = chatInput.val().trim();
+    if (message !== '') {
+      sendMessage(message);
+      chatInput.val('');
+    }
+  });
+  function sendMessage(message) {
+    var userMessageElement = createMessageElement('Usuario', message);
+    chatMessages.append(userMessageElement);
+    chatHistory.push({
+      role: 'user',
+      content: message
     });
-    
-    function sendMessage(message) {
-        var userMessageElement = createMessageElement('Usuario', message);
-        chatMessages.append(userMessageElement);
-
-        // Actualizar el historial de mensajes con el mensaje del usuario
-        chatHistory.push({ role: 'user', content: message });
-    
-        // Mostrar el preloader en el globo del bot
-        var botMessageElement = createBotMessageElement('');
-        chatMessages.append(botMessageElement);
-        chatMessages.scrollTop(chatMessages[0].scrollHeight);
-    
-        $.ajax({
-            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-            url: '/open-ai',
-            method: 'POST',
-            data: {
-                msg: message,
-                chatHistory: chatHistory, // Pasar el historial de mensajes al servidor
-                // Asumimos que las claves están configuradas en el servidor y no las pasamos aquí
-                // apiKey: apiKey,
-                // elevenApiKey: elevenApiKey
-            },
-            success: function(response) {
-                // Actualizar el historial de mensajes con el mensaje del asistente
-                chatHistory.push({ role: 'assistant', content: response.response });
-
-                // Actualizar el mensaje en el mismo globo del bot
-                botMessageElement.text(response.response);
-                if (response.audioFile) {
-                    fetch(response.audioFile) // Realiza una solicitud para obtener el archivo de audio
-                        .then(response => response.blob()) // Convierte la respuesta en un objeto Blob
-                        .then(blob => {
-                            var audioUrl = URL.createObjectURL(blob); // Crea una URL para el objeto Blob obtenido
-                                
-                            var audioElement = document.createElement("audio"); // Crea un elemento de audio
-                            audioElement.src = audioUrl; // Establece la fuente del audio como la URL creada
-                            audioElement.autoplay = true; // Habilita la reproducción automática del audio
-                                
-                            botMessageElement.append(audioElement); // Agrega el elemento de audio al elemento de mensaje de bot
-                        })
-                        .catch(error => {
-                            console.error("Error al obtener el archivo de audio:", error); // Muestra un mensaje de error en caso de fallo
-                        });
-                }
-                chatMessages.scrollTop(chatMessages[0].scrollHeight);
-            },
-            error: function(xhr, status, error) {
-                // Actualizar el mensaje de error en el mismo globo del bot
-                botMessageElement.text("ERROR: O no has puesto tu clave de API o GPT no funciona en este momento.");
-                chatMessages.scrollTop(chatMessages[0].scrollHeight);
-            }
+    var botMessageElement = createBotMessageElement('');
+    chatMessages.append(botMessageElement);
+    chatMessages.scrollTop(chatMessages[0].scrollHeight);
+    $.ajax({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      },
+      url: '/open-ai',
+      method: 'POST',
+      data: {
+        msg: message,
+        chatHistory: chatHistory
+      },
+      success: function success(response) {
+        chatHistory.push({
+          role: 'assistant',
+          content: response.response
         });
-    }
-    
-    // Crea un elemento de mensaje en función del remitente y el mensaje proporcionados.
-    function createMessageElement(sender, message) {
-        var senderClass = sender.toLowerCase() === 'usuario' ? 'user-message' : 'bot-message';
-        var messageElement = $('<div class="' + senderClass + '">'); // Crea un elemento div con la clase del remitente
-        messageElement.text(message); // Establece el texto del mensaje en el elemento
-        return messageElement; // Retorna el elemento de mensaje creado
-    }
-    
-    //crea un elemento de mensaje de bot. 
-    function createBotMessageElement(message) {
-        var botMessageElement = $('<div class="bot-message">'); // Crea un elemento div con la clase "bot-message"
-        var preloaderImage = $('<img src="assets/img/preloaders/preloader.gif" width="32" alt="Preloader" />');
-        botMessageElement.append(preloaderImage); // Agrega la imagen de carga al elemento de mensaje de bot
-        botMessageElement.append('<span>' + message + '</span>'); // Agrega el mensaje al elemento de mensaje de bot
-        return botMessageElement; // Retorna el elemento de mensaje de bot creado
-    }
+        botMessageElement.find('.chat-message-text p').html(response.response);
+        if (response.audioFile) {
+          fetch(response.audioFile).then(function (response) {
+            return response.blob();
+          }).then(function (blob) {
+            var audioUrl = URL.createObjectURL(blob);
+            var audioElement = document.createElement("audio");
+            audioElement.src = audioUrl;
+            audioElement.autoplay = true;
+            botMessageElement.find('.chat-message-text').append(audioElement);
+          })["catch"](function (error) {
+            console.error("Error al obtener el archivo de audio:", error);
+          });
+        }
+        chatMessages.scrollTop(chatMessages[0].scrollHeight);
+      },
+      error: function error(xhr, status, _error) {
+        botMessageElement.find('.chat-message-text p').html("ERROR: O no has puesto tu clave de API o GPT no funciona en este momento.");
+        chatMessages.scrollTop(chatMessages[0].scrollHeight);
+      }
+    });
+  }
+  function createMessageElement(sender, message) {
+    var isUser = sender.toLowerCase() === 'usuario';
+    var senderClass = isUser ? 'chat-message chat-message-right' : 'chat-message';
+    var avatarUrl = isUser ? '/assets/img/avatars/guru-meditating.jpg' : '/assets/img/avatars/guru-meditating.jpg';
+    var messageElement = $('<li class="' + senderClass + '">');
+    var messageContent = "\n            <div class=\"d-flex overflow-hidden\">\n                ".concat(isUser ? '' : "\n                <div class=\"user-avatar flex-shrink-0 me-3\">\n                    <div class=\"avatar avatar-sm\">\n                        <img src=\"".concat(avatarUrl, "\" alt=\"Avatar\" class=\"rounded-circle\">\n                    </div>\n                </div>"), "\n                <div class=\"chat-message-wrapper flex-grow-1\">\n                    <div class=\"chat-message-text\">\n                        <p class=\"mb-0\">").concat(message, "</p>\n                    </div>\n                    <div class=\"").concat(isUser ? 'text-end ' : '', "text-muted mt-1\">\n                        ").concat(isUser ? "<i class='ti ti-checks ti-xs me-1 text-success'></i>" : '', "\n                        <small>").concat(new Date().toLocaleTimeString(), "</small>\n                    </div>\n                </div>\n                ").concat(isUser ? "\n                <div class=\"user-avatar flex-shrink-0 ms-3\">\n                    <div class=\"avatar avatar-sm\">\n                        <img src=\"".concat(avatarUrl, "\" alt=\"Avatar\" class=\"rounded-circle\">\n                    </div>\n                </div>") : '', "\n            </div>\n        ");
+    messageElement.html(messageContent);
+    return messageElement;
+  }
+  function createBotMessageElement(message) {
+    var botMessageElement = $('<li class="chat-message">');
+    var avatarUrl = '/assets/img/avatars/guru-meditating.jpg';
+    var preloaderImage = '<div class="sk-wave sk-primary"><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div><div class="sk-wave-rect"></div></div>';
+    var messageContent = "\n            <div class=\"d-flex overflow-hidden\">\n                <div class=\"user-avatar flex-shrink-0 me-3\">\n                    <div class=\"avatar avatar-sm\">\n                        <img src=\"".concat(avatarUrl, "\" alt=\"Avatar\" class=\"rounded-circle\">\n                    </div>\n                </div>\n                <div class=\"chat-message-wrapper flex-grow-1\">\n                    <div class=\"chat-message-text\">\n                        ").concat(preloaderImage, "\n                        <p class=\"mb-0\">").concat(message, "</p>\n                    </div>\n                    <div class=\"text-muted mt-1\">\n                        <small>").concat(new Date().toLocaleTimeString(), "</small>\n                    </div>\n                </div>\n            </div>\n        ");
+    botMessageElement.html(messageContent);
+    return botMessageElement;
+  }
+});
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
 });
--- resources/views/ai/index.blade.php
+++ resources/views/ai/index.blade.php
@@ -1,53 +1,74 @@
 @extends('layouts/layoutMaster')
 
-@section('title', 'AI')
+@section('title', 'Chat - Apps')
 
 @section('vendor-style')
-<link rel="stylesheet" href="{{asset('assets/vendor/libs/flatpickr/flatpickr.css')}}" />
-<link rel="stylesheet" href="{{asset('assets/vendor/libs/select2/select2.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/openaai.css') }}">
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/bootstrap-maxlength/bootstrap-maxlength.css')}}" />
+@endsection
+
+@section('page-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/css/pages/app-chat.css')}}" />
 @endsection
 
 @section('vendor-script')
-<script src="{{asset('assets/vendor/libs/cleavejs/cleave.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/cleavejs/cleave-phone.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/moment/moment.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/flatpickr/flatpickr.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/select2/select2.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/bootstrap-maxlength/bootstrap-maxlength.js')}}"></script>
 @endsection
 
 @section('page-script')
-<script src="{{asset('assets/js/form-layouts.js')}}"></script>
+<script src="{{asset('assets/js/app-chat.js')}}"></script>
 @endsection
 
 @section('content')
-<div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center mb-3">
-    <div class="d-flex flex-column justify-content-center">
-        <h4 class="mb-1 mt-3">Helpdesk</h4>
-        <p class="text-muted">Manage your messages with ease and keep your audience engaged!</p>
-    </div>
-</div>
-
-<div class="card mb-4">
-    <h5 class="card-header">Messages</h5>
-    <div id="chat-container">
-        <div id="chat-messages" class="chat-container"></div>
-        <form id="chat-form" method="POST" class="form-send-message d-flex justify-content-between align-items-center p-3">
-            @csrf
-            <input class="form-control message-input border-0 me-3 shadow-none" id="chat-input" placeholder="No tengo tiempo para tus tonterías, ¿qué quieres?" />
-            <div class="message-actions d-flex align-items-center">
-                <i class="speech-to-text ti ti-microphone ti-sm cursor-pointer"></i>
-                <label for="attach-doc" class="form-label mb-0">
-                    <i class="ti ti-photo ti-sm cursor-pointer mx-3"></i>
-                    <input type="file" id="attach-doc" hidden="">
-                </label>
-                <button class="btn btn-primary d-flex send-msg-btn waves-effect waves-light" type="submit">
-                    <i class="ti ti-send me-md-1 me-0"></i>
-                    <span class="align-middle d-md-inline-block d-none">Send</span>
-                </button>
+<div class="app-chat card overflow-hidden">
+  <div class="row g-0">
+    <!-- Chat History -->
+    <div class="col app-chat-history bg-body">
+      <div class="chat-history-wrapper">
+        <div class="chat-history-header border-bottom">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex overflow-hidden align-items-center">
+              <i class="ti ti-menu-2 ti-sm cursor-pointer d-lg-none d-block me-2" data-bs-toggle="sidebar" data-overlay data-target="#app-chat-contacts"></i>
+              <div class="flex-shrink-0 avatar">
+                <img src="{{asset('assets/img/avatars/guru-meditating.jpg')}}" alt="Avatar" class="rounded-circle" data-bs-toggle="sidebar" data-overlay data-target="#app-chat-sidebar-right">
+              </div>
+              <div class="chat-contact-info flex-grow-1 ms-2">
+                <h6 class="m-0">ChatBot</h6>
+                <small class="user-status text-muted">Open AI</small>
+              </div>
             </div>
-        </form>
+          </div>
+        </div>
+        <div class="chat-history-body bg-body">
+          <ul class="list-unstyled chat-history">
+            
+            <div id="chat-messages" class="chat-container" class="chat-history-wrapperx"></div>
+
+          </ul>
+        </div>
+        <!-- Chat message form -->
+        <div class="chat-history-footer shadow-sm">
+          <form id="chat-form" method="POST" class="form-send-message d-flex justify-content-between align-items-center ">
+            @csrf
+            <input id="chat-input" class="form-control message-input border-0 me-3 shadow-none" placeholder="Type your message here">
+            <div class="message-actions d-flex align-items-center">
+              <i class="speech-to-text ti ti-microphone ti-sm cursor-pointer"></i>
+              <label for="attach-doc" class="form-label mb-0">
+                <i class="ti ti-photo ti-sm cursor-pointer mx-3"></i>
+                <input type="file" id="attach-doc" hidden>
+              </label>
+              <button class="btn btn-primary d-flex send-msg-btn">
+                <i class="ti ti-send me-md-1 me-0"></i>
+                <span class="align-middle d-md-inline-block d-none">Send</span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+    <!-- /Chat History -->
+
+    <div class="app-overlay"></div>
+  </div>
 </div>
 
 <script src="{{ asset(mix('assets/vendor/libs/jquery/jquery.js')) }}"></script>
