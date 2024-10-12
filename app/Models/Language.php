ecdc206b40f0520ec5ebda9fc2f0c2ed8355cf8e
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2024-10-12 09:27:21 +0200

    Fix Language timestamps

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Language extends Model
 {
     protected $primaryKey = 'code';
+
+    public $timestamps = false;
+
     public $incrementing = false;
+    
     protected $keyType = 'string';
 
     protected $fillable = ['code', 'name'];
