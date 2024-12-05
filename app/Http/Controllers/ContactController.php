2562ced6ef22ee8c152d7faa927bdbb7aec624fc
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2024-12-05 12:59:48 +0100

    Auto Mapping

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -557,6 +557,17 @@ class ContactController extends Controller
 		$rows = $worksheet->toArray();
 		$headers = array_shift($rows);
 
+		$rows = array_filter($rows, function ($row)
+		{
+			return array_filter($row, function ($cell)
+			{
+				return !empty($cell) && $cell !== '' && $cell !== null;
+			});
+		});
+
+		$rows = array_values($rows);
+		shuffle($rows);
+
 		$availableFields = [
 			'name' => 'Nombre',
 			'email' => 'Email',
@@ -612,7 +623,7 @@ class ContactController extends Controller
 				}
 			}
 
-			if (!empty($mappedRow) || !empty($sources))
+			if (!empty($mappedRow['name']))
 			{
 				$contact = Contact::create(array_merge($mappedRow, [
 					'team_id' => auth()->user()->currentTeam->id,
