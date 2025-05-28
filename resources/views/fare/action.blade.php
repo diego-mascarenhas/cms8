57858fbb06195b41a3fa7809a494a2a450ec5429
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-05-28 15:54:08 +0200

    Refactor action buttons in multiple views

--- resources/views/fare/action.blade.php
+++ resources/views/fare/action.blade.php
@@ -1,5 +1,5 @@
 <div class="d-inline-flex">
-    <a href="{{ route('fare.edit', $fare->id) }}" class="btn btn-icon btn-sm btn-text-secondary rounded-pill">
-        <i class="ti ti-pencil"></i>
-    </a>
+	<a href="{{ route('fare.edit', $fare->id) }}" class="btn btn-sm btn-icon item-edit me-1">
+		<i class="ti ti-edit ti-sm me-2"></i>
+	</a>
 </div>
--- resources/views/language/variants/action.blade.php
+++ resources/views/language/variants/action.blade.php
@@ -1,13 +1,5 @@
 <div class="d-inline-flex">
-  <a href="{{ route('language-variants.edit', $variant->id) }}" class="btn btn-sm btn-icon item-edit me-1">
-    <i class="ti ti-edit ti-sm me-2"></i>
-  </a>
-  <form action="{{ route('language-variants.destroy', $variant->id) }}" method="POST" class="delete-form">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-sm btn-icon item-delete"
-      onclick="return confirm('¿Está seguro de eliminar esta variante?')">
-      <i class="ti ti-trash ti-sm"></i>
-    </button>
-  </form>
+	<a href="{{ route('language-variants.edit', $variant->id) }}" class="btn btn-sm btn-icon item-edit me-1">
+		<i class="ti ti-edit ti-sm me-2"></i>
+	</a>
 </div>
