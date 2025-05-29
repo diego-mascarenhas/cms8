c1d82289b0f1c1e2f14f0de67d85ba4aa38edb8e
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-05-29 12:34:53 +0200

    Remove fare_unit migration file

---
+++ database/migrations/2025_05_06_000001_create_stylebooks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stylebooks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained();
+            $table->string('name', 255);
+            $table->string('file', 255);
+            $table->char('language', 2);
+            $table->dateTime('date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stylebooks');
+    }
+};
