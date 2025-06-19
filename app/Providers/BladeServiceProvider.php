93f456719c6cdda4eedc7920a577bdc1f1cb8cbf
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-06-19 09:48:30 +0200

    Add Blade components registration

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\View\Components\ClientSelect;
+use App\View\Components\CountrySelect;
+use App\View\Components\EnterpriseStatusSelect;
 use App\View\Components\FareSelect;
+use App\View\Components\LanguageSelect;
+use App\View\Components\TaskNotifications;
+use App\View\Components\TeamUsersSelect;
+use App\View\Components\VariantLanguageSelect;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 
@@ -12,6 +19,13 @@ class BladeServiceProvider extends ServiceProvider
     {
         // Register Blade components
         Blade::component('fare-select', FareSelect::class);
+        Blade::component('variant-language-select', VariantLanguageSelect::class);
+        Blade::component('client-select', ClientSelect::class);
+        Blade::component('country-select', CountrySelect::class);
+        Blade::component('enterprise-status-select', EnterpriseStatusSelect::class);
+        Blade::component('language-select', LanguageSelect::class);
+        Blade::component('team-users-select', TeamUsersSelect::class);
+        Blade::component('task-notifications', TaskNotifications::class);
         
         // Register Blade directives
         Blade::directive('formatMinutes', function ($expression) {
