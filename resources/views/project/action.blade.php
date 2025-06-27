426143b2abe4d8b59fa3fb2b0c5e384f1a5efa3f
Diego Mascarenhas <diego@revisionalpha.com>
2025-06-27 10:00:13 +0200

    Update project action view to replace edit link with show link and adjust icon classes for consistency.

--- resources/views/project/action.blade.php
+++ resources/views/project/action.blade.php
@@ -1,3 +1,4 @@
 <div class="d-flex justify-content-center align-items-center">
-    <a href="{{ route('project.edit', $id) }}" class="test-bodyx"><i class="ti ti-edit ti-sm me-2"></i></a>
+    <a href="{{ route('project.show', $id) }}" class="text-body"><i class="ti ti-eye ti-sm me-2"></i></a>
+    <a href="{{ route('project.edit', $id) }}" class="text-body"><i class="ti ti-edit ti-sm me-2"></i></a>
 </div>
