93a3e0c5399aeeb0af8ce32392c57f0545c899f4
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-07-23 14:16:51 +0200

    Add team-based visibility for task and chat notifications

--- resources/views/livewire/help-center-icon.blade.php
+++ resources/views/livewire/help-center-icon.blade.php
@@ -1,10 +1,12 @@
 <div wire:poll>
-    <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1">
-        <a class="nav-link dropdown-toggle hide-arrow" href="{{ route('chat.index') }}">
-            <i class="ti ti-lifebuoy ti-md"></i>
-            @if ($inboundCount > 0)
-                <span class="badge bg-danger rounded-pill badge-notifications">{{ $inboundCount }}</span>
-            @endif
-        </a>
-    </li>
+    @if(auth()->user()->currentTeam && auth()->user()->currentTeam->hasModule('chat'))
+        <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1">
+            <a class="nav-link dropdown-toggle hide-arrow" href="{{ route('chat.index') }}">
+                <i class="ti ti-lifebuoy ti-md"></i>
+                @if ($inboundCount > 0)
+                    <span class="badge bg-danger rounded-pill badge-notifications">{{ $inboundCount }}</span>
+                @endif
+            </a>
+        </li>
+    @endif
 </div>
