e8c6d9502e35b50d9a331f47f809e07e105cbea4
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-07-28 11:32:04 +0200

    Change error page layout and translate texts

--- resources/views/content/pages/pages-misc-error.blade.php
+++ resources/views/content/pages/pages-misc-error.blade.php
@@ -3,7 +3,7 @@ $customizerHidden = 'customizer-hide';
 $configData = Helper::appClasses();
 @endphp
 
-@extends('layouts/layoutMaster')
+@extends('layouts/blankLayout')
 
 @section('title', 'Error - Pages')
 
@@ -13,13 +13,13 @@ $configData = Helper::appClasses();
 @endsection
 
 
-@section('content')
+@section('layoutContent')
 <!-- Error -->
 <div class="container-xxl container-p-y">
   <div class="misc-wrapper">
-    <h2 class="mb-1 mt-4">Page Not Found :(</h2>
-    <p class="mb-4 mx-2">Oops! 😖 The requested URL was not found on this server.</p>
-    <a href="{{url('/')}}" class="btn btn-primary mb-4">Back to home</a>
+    <h2 class="mb-1 mt-4">Página No Encontrada :(</h2>
+    <p class="mb-4 mx-2">¡Ups! 😖 La URL solicitada no se encontró en este servidor.</p>
+    <a href="{{url('/')}}" class="btn btn-primary mb-4">Volver al inicio</a>
     <div class="mt-4">
       <img src="{{ asset('assets/img/illustrations/page-misc-error.png') }}" alt="page-misc-error" width="225" class="img-fluid">
     </div>
