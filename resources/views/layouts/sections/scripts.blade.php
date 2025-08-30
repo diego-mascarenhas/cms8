d4fdec00193719bacb82b1f55880cdbcfb5fdfc6
Diego Mascarenhas <diego@revisionalpha.com>
2025-08-30 17:50:06 +0200

    refactor: Update asset paths in scripts and styles for humano-core integration

--- resources/views/layouts/sections/scripts.blade.php
+++ resources/views/layouts/sections/scripts.blade.php
@@ -1,16 +1,16 @@
 <!-- BEGIN: Vendor JS-->
-<script src="{{ asset(mix('assets/vendor/libs/jquery/jquery.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/libs/popper/popper.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/js/bootstrap.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/libs/node-waves/node-waves.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/libs/hammer/hammer.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/libs/typeahead-js/typeahead.js')) }}"></script>
-<script src="{{ asset(mix('assets/vendor/js/menu.js')) }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/libs/jquery/jquery.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/libs/popper/popper.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/js/bootstrap.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/libs/hammer/hammer.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/vendor/js/menu.js') }}"></script>
 @yield('vendor-script')
 <!-- END: Page Vendor JS-->
 <!-- BEGIN: Theme JS-->
-<script src="{{ asset(mix('assets/js/main.js')) }}"></script>
+<script src="{{ asset('vendor/humano-core/assets/js/main.js') }}"></script>
 
 <!-- END: Theme JS-->
 <!-- Pricing Modal JS-->
