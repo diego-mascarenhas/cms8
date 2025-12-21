397398c1899e177ccd9e2fcbac659f984ca70a53
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-12-21 10:51:56 +0100

    Remove Mailgun integration and related configurations, updating email provider settings to use SMTP with Gmail. Refactor email handling traits and remove unused SendGrid mail class to streamline email functionality.

--- config/services.php
+++ config/services.php
@@ -8,18 +8,11 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | as Postmark, AWS and more. This file provides the de facto
     | location for this type of information, allowing packages to have
     | a conventional file to locate the various service credentials.
     |
     */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
-    ],
 
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
@@ -77,7 +70,7 @@ return [
     ],
 
     'email' => [
-        'provider' => env('EMAIL_PROVIDER', 'smtp'), // mailbaby | mailgun | smtp
+        'provider' => env('EMAIL_PROVIDER', 'smtp'), // mailbaby | smtp
         'fallback_to_smtp' => env('EMAIL_FALLBACK_TO_SMTP', true),
 
         // Email sending delay configuration
