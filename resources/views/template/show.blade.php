366aaadab789ccb37f92b656e554cdd69eda2485
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-12-22 09:55:05 +0100

    Remove activity logging from DatabaseSeeder and update template view for cleaner HTML structure.

--- resources/views/template/show.blade.php
+++ resources/views/template/show.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>{{ $page->name }} - Preview</title>
-	
+
 	<style>
 		{!! $page->gjs_data['css'] ?? '' !!}
 	</style>
